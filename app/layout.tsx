fbd87edc77da5b1aa668b05821ffc0cb2669d3b7
Adarsh Singh <adarsh.kiet@gmail.com>
2026-03-06 16:37:51 +0530

    error mixed layoufvefverevrevt

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin="" />
-      </head>
       <body className={`${inter.variable} ${outfit.variable} font-sans`}>
         <Navbar />
         {children}
